34d3b8a5d2f2de25fd4a4761538c40d01d6084c3
Alexander Schelchere <ascellie@gmail.com>
2019-07-20 19:48:34 -0400

    find user

--- js/index.js
+++ js/index.js
@@ -3,16 +3,18 @@ console.log('Running...');
 // GET - URL endpoints
 const BASE_URL = 'https://api.github.com/';
 const SEARCH_QUERY_URL = `${BASE_URL}search/users?q=`;
-const USER_REPOS_HEAD = `${BASE_URL}users/`;
-const USER_REPOS_TAIL = '/repos';
+
+// NOT NEEDED, repos_url available upon search.
+// const USER_REPOS_HEAD = `${BASE_URL}users/`;
+// const USER_REPOS_TAIL = '/repos';
 
 // Fire these once the dom has loaded.
 document.addEventListener('DOMContentLoaded', event => {
-  formHandler();
+  handleForm();
 });
 
 // Funtion handles submit on form
-function formHandler() {
+function handleForm() {
   // find form
   let form = document.querySelector('#github-form');
   // add event listener
@@ -22,12 +24,13 @@ function formHandler() {
     // get a hold of that value
     let inputVal = event.target.search.value;
     // call our search handling function
-    searchHandler(inputVal);
+    handleSearch(inputVal);
   });
 }
 
 // Funtion handles username search
-function searchHandler(username) {
+function handleSearch(username) {
+  // Fetch our search query and concatenate link with the username from form.
   fetch(`${SEARCH_QUERY_URL + username}`, {
     method: 'GET',
     headers: {
@@ -36,18 +39,50 @@ function searchHandler(username) {
       Accept: 'application/vnd.github.v3+json'
     }
   })
+    // Parse response into JSON
     .then(res => res.json())
-    .then(data => console.log(data));
+    // Call our git hub user handling function
+    // Pass the username so we can compare it with the search
+    .then(data => handleGitHubUser(data, username));
 }
 
-// Responds with
-/*
-{total_count: 1, incomplete_results: false, items: Array(1)}
-incomplete_results: false
-items: Array(1)
-0: {login: "Lexscher", id: 35089689, node_id: "MDQ6VXNlcjM1MDg5Njg5", avatar_url: "https://avatars3.githubusercontent.com/u/35089689?v=4", gravatar_id: "", …}
-length: 1
-__proto__: Array(0)
-total_count: 1
+// This will parse through our search, find the exact match, then fetch our user's Repos
+function handleGitHubUser(users, searchedName) {
+  // check our response status
+  let invalidResponse = users.incomplete_results; // false if we have a valid response
+  // if we have an invalid response, return the error
+  if (invalidResponse) {
+    return 'INVALID SEARCH';
+  }
+  // get the array of users
+  let usersArr = users.items;
+  // find the one we're looking for
+  let foundUser = usersArr.find(
+    user => user.login.toLowerCase() === searchedName.toLowerCase()
+  );
+
+  console.log(foundUser);
+}
+/* RESPONSE
+{login: "Lexscher", id: 35089689, node_id: "MDQ6VXNlcjM1MDg5Njg5", avatar_url: "https://avatars3.githubusercontent.com/u/35089689?v=4", gravatar_id: "", …}
+avatar_url: "https://avatars3.githubusercontent.com/u/35089689?v=4"
+events_url: "https://api.github.com/users/Lexscher/events{/privacy}"
+followers_url: "https://api.github.com/users/Lexscher/followers"
+following_url: "https://api.github.com/users/Lexscher/following{/other_user}"
+gists_url: "https://api.github.com/users/Lexscher/gists{/gist_id}"
+gravatar_id: ""
+html_url: "https://github.com/Lexscher"
+id: 35089689
+login: "Lexscher"
+node_id: "MDQ6VXNlcjM1MDg5Njg5"
+organizations_url: "https://api.github.com/users/Lexscher/orgs"
+received_events_url: "https://api.github.com/users/Lexscher/received_events"
+repos_url: "https://api.github.com/users/Lexscher/repos"
+score: 112.73647
+site_admin: false
+starred_url: "https://api.github.com/users/Lexscher/starred{/owner}{/repo}"
+subscriptions_url: "https://api.github.com/users/Lexscher/subscriptions"
+type: "User"
+url: "https://api.github.com/users/Lexscher"
 __proto__: Object
 */
