15a1c52567b03e0c2d4fa4cae9f8401dc0238db1
Alexander Schelchere <ascellie@gmail.com>
2019-07-20 19:55:49 -0400

    fetch repos

--- js/index.js
+++ js/index.js
@@ -60,29 +60,107 @@ function handleGitHubUser(users, searchedName) {
   let foundUser = usersArr.find(
     user => user.login.toLowerCase() === searchedName.toLowerCase()
   );
-
-  console.log(foundUser);
+  // get user repos endpoint
+  let userReposUrl = foundUser.repos_url;
+  // call our fetch repos method
+  fetchRepos(userReposUrl);
 }
-/* RESPONSE
-{login: "Lexscher", id: 35089689, node_id: "MDQ6VXNlcjM1MDg5Njg5", avatar_url: "https://avatars3.githubusercontent.com/u/35089689?v=4", gravatar_id: "", …}
-avatar_url: "https://avatars3.githubusercontent.com/u/35089689?v=4"
-events_url: "https://api.github.com/users/Lexscher/events{/privacy}"
-followers_url: "https://api.github.com/users/Lexscher/followers"
-following_url: "https://api.github.com/users/Lexscher/following{/other_user}"
-gists_url: "https://api.github.com/users/Lexscher/gists{/gist_id}"
-gravatar_id: ""
-html_url: "https://github.com/Lexscher"
-id: 35089689
-login: "Lexscher"
-node_id: "MDQ6VXNlcjM1MDg5Njg5"
-organizations_url: "https://api.github.com/users/Lexscher/orgs"
-received_events_url: "https://api.github.com/users/Lexscher/received_events"
-repos_url: "https://api.github.com/users/Lexscher/repos"
-score: 112.73647
-site_admin: false
-starred_url: "https://api.github.com/users/Lexscher/starred{/owner}{/repo}"
-subscriptions_url: "https://api.github.com/users/Lexscher/subscriptions"
-type: "User"
-url: "https://api.github.com/users/Lexscher"
+
+function fetchRepos(reposUrl) {
+  console.log(reposUrl);
+  // fetch url
+  fetch(reposUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      // Doesn't require a key but they ask for us to add this in our headers.
+      Accept: 'application/vnd.github.v3+json'
+    }
+  })
+    // Parse response into JSON
+    .then(res => res.json())
+    // call our handleRepos function
+    .then(handleRepos);
+}
+
+function handleRepos(repoListData) {
+  console.log(repoListData);
+}
+
+/* RESULTS - AN ARRAY OF OBJECTS, THIS IS THE FIRST ONE:
+0:
+archive_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/{archive_format}{/ref}"
+archived: false
+assignees_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/assignees{/user}"
+blobs_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/git/blobs{/sha}"
+branches_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/branches{/branch}"
+clone_url: "https://github.com/Lexscher/activerecord-validations-lab-dumbo-web-060319.git"
+collaborators_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/collaborators{/collaborator}"
+comments_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/comments{/number}"
+commits_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/commits{/sha}"
+compare_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/compare/{base}...{head}"
+contents_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/contents/{+path}"
+contributors_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/contributors"
+created_at: "2019-06-30T23:07:05Z"
+default_branch: "master"
+deployments_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/deployments"
+description: null
+disabled: false
+downloads_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/downloads"
+events_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/events"
+fork: true
+forks: 0
+forks_count: 0
+forks_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/forks"
+full_name: "Lexscher/activerecord-validations-lab-dumbo-web-060319"
+git_commits_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/git/commits{/sha}"
+git_refs_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/git/refs{/sha}"
+git_tags_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/git/tags{/sha}"
+git_url: "git://github.com/Lexscher/activerecord-validations-lab-dumbo-web-060319.git"
+has_downloads: true
+has_issues: false
+has_pages: false
+has_projects: true
+has_wiki: false
+homepage: null
+hooks_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/hooks"
+html_url: "https://github.com/Lexscher/activerecord-validations-lab-dumbo-web-060319"
+id: 194569516
+issue_comment_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/issues/comments{/number}"
+issue_events_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/issues/events{/number}"
+issues_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/issues{/number}"
+keys_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/keys{/key_id}"
+labels_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/labels{/name}"
+language: "Ruby"
+languages_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/languages"
+license: null
+merges_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/merges"
+milestones_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/milestones{/number}"
+mirror_url: null
+name: "activerecord-validations-lab-dumbo-web-060319"
+node_id: "MDEwOlJlcG9zaXRvcnkxOTQ1Njk1MTY="
+notifications_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/notifications{?since,all,participating}"
+open_issues: 0
+open_issues_count: 0
+owner: {login: "Lexscher", id: 35089689, node_id: "MDQ6VXNlcjM1MDg5Njg5", avatar_url: "https://avatars3.githubusercontent.com/u/35089689?v=4", gravatar_id: "", …}
+private: false
+pulls_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/pulls{/number}"
+pushed_at: "2019-07-01T03:47:09Z"
+releases_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/releases{/id}"
+size: 47
+ssh_url: "git@github.com:Lexscher/activerecord-validations-lab-dumbo-web-060319.git"
+stargazers_count: 0
+stargazers_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/stargazers"
+statuses_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/statuses/{sha}"
+subscribers_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/subscribers"
+subscription_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/subscription"
+svn_url: "https://github.com/Lexscher/activerecord-validations-lab-dumbo-web-060319"
+tags_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/tags"
+teams_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/teams"
+trees_url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319/git/trees{/sha}"
+updated_at: "2019-07-01T03:47:10Z"
+url: "https://api.github.com/repos/Lexscher/activerecord-validations-lab-dumbo-web-060319"
+watchers: 0
+watchers_count: 0
 __proto__: Object
-*/
+ */
